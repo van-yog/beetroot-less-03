ca242164f9179729d90b80a5dc013dcb9d537c05
van-yog <ikharchenko2008@gmail.com>
2021-02-22 10:53:36 +0200

    optimaze homework

--- src/js/players.js
+++ src/js/players.js
@@ -1,5 +1,4 @@
 import { generate as id } from "shortid";
-// import { createAction } from "@reduxjs/toolkit";
 import { Dispatcher, ReduceStore } from "./flux";
 import { data } from "./data";
 
@@ -9,36 +8,30 @@ const SELECT_STATUS = "SELECT_STATUS";
 
 const newPlayerAction = (name) => ({
   type: NEW_PLAYER,
-  payload: { id: id(), name, result: 0, status: 1 },
+  payload: { name },
 });
 
-const selectStatusAction = (status) => {
-  console.log("selectStatusAction->status:", status);
-  return {
-    type: SELECT_STATUS,
-    payload: { status },
-  };
-};
+const selectStatusAction = (status) => ({
+  type: SELECT_STATUS,
+  payload: { status },
+});
 
 /* ======== store ===========   */
 
 class PlayersReducerStore extends ReduceStore {
   getInitialState() {
-    let initState = localStorage["preference"]
-      ? JSON.parse(localStorage["preference"])
-      : { players: data.players, status: 0 };
-    console.log("PlayersReducerStore -> getInitialState ", initState);
+    let initState = localStorage["players"]
+      ? JSON.parse(localStorage["players"])
+      : { players: data.players, status: -1 };
 
     return initState;
   }
   reducer(state, action) {
-    console.log("PlayersReducerStore -> reducer ", action);
     const { type, payload } = action;
     let newState;
 
     switch (type) {
       case NEW_PLAYER:
-        console.log("NEW_PLAYER");
         newState = { ...state, players: [...state.players] };
         newState.players.push({
           id: id(),
@@ -48,7 +41,6 @@ class PlayersReducerStore extends ReduceStore {
         });
         return newState;
       case SELECT_STATUS:
-        console.log("SELECT_STATUS");
         newState = { ...state, players: [...state.players] };
         newState.status = payload.status;
         return newState;
@@ -72,32 +64,40 @@ data.statuses.forEach(
     (statusSelect.innerHTML += `<option value="${status.id}">${status.title}</option>`)
 );
 
+statusSelect.value = playerStore.getState().status;
+
 const results = document.getElementById("results");
 
 function render(state) {
-  console.log("Render->state'", state);
   results.innerHTML = "";
   state.players.forEach((player, idx) => {
     const { name, result, status } = player;
-    console.log("statusSelect.value ", statusSelect.value);
-    console.log("data.statuses[status - 1]", data.statuses[status - 1].id);
-    console.log(+statusSelect.value === data.statuses[status - 1].id);
+
     results.innerHTML += `
-      <tr class=${
-        +statusSelect.value === data.statuses[status - 1].id ? "table-info" : ""
-      }>
+      <tr data-status="${status}">
           <td>${idx + 1}</td>
           <td>${name}</td>
           <td>${result}</td>
-          <td >${data.statuses[status - 1].title}</td>
+          <td  >${data.statuses[status - 1].title}</td>
       </tr>
       `;
   });
-  localStorage["preference"] = JSON.stringify(state);
+  localStorage["players"] = JSON.stringify(state);
+}
+
+function renderStatus({ players, status }) {
+  let tableRow = document.querySelectorAll("tr");
+
+  tableRow.forEach(
+    (tr) => (tr.className = tr.dataset.status === status ? "table-info" : "")
+  );
 }
 
 render(playerStore.getState());
+renderStatus(playerStore.getState());
+
 playerStore.subscribe(render);
+playerStore.subscribe(renderStatus);
 
 /* ======== handlers ===========   */
 
@@ -105,7 +105,6 @@ document.addEventListener("submit", handlePlayers);
 document.addEventListener("change", handlePlayers);
 
 function handlePlayers({ target, type }) {
-  console.log("handlePlayers !");
   switch (type) {
     case "submit":
       event.preventDefault();
@@ -115,7 +114,6 @@ function handlePlayers({ target, type }) {
       }
       target[0].value = "";
       playerDispatcher.dispatch(newPlayerAction(newPlayer));
-      console.log(newPlayerAction(newPlayer));
 
       break;
     case "change":
@@ -123,8 +121,6 @@ function handlePlayers({ target, type }) {
         return;
       }
       const status = target.value;
-      console.log("CHANGE, VALUE ", status);
       playerDispatcher.dispatch(selectStatusAction(status));
-      console.log(selectStatusAction(status));
   }
 }
